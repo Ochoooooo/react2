cfa8015646b7e31158356d6f7089228cc6f70f1e
Ochoooooo <greenfieldrp7@gmail.com>
2024-12-16 10:22:38 +0100

    props.1

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,25 +1,14 @@
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
-const pokemon = pokemonList[1];
-function PokemonCard() {
+export default function PokemonCard({ pokemon }) {
   return (
     <figure>
-      {pokemon.imgSrc ? (
-        <img src={pokemon.imgSrc} alt={pokemon.name} />
+      {pokemon.imgSrc && pokemon.name ? (
+        <div>
+          <img src={pokemon.imgSrc} alt={pokemon.name} />
+          <p>{pokemon.name}</p>
+        </div>
       ) : (
-        <p>"???"</p>
+        <p>???</p>
       )}
-      <figcaption>{pokemon.name}</figcaption>
     </figure>
   );
 }
-
-export default PokemonCard;
